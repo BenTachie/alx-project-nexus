15cd3ec9894a95860534a4158b4bc86ff2933e6e
BenTachie <tachiebenedict@gmail.com>
2025-09-28 00:24:58 +0200

    feat:  update ProductCard component to Handle Dynamic Data

--- glowa-catalog/components/ProductCard.tsx
+++ glowa-catalog/components/ProductCard.tsx
@@ -10,9 +10,17 @@ type Props = {
 export default function ProductCard({ name, desc, price, image }: Props) {
   return (
     <View className="bg-gray-100 rounded-xl w-40 p-3">
-      <Image source={{ uri: image }} className="w-full h-32 rounded-lg" />
-      <Text className="mt-2 font-semibold">{name}</Text>
-      <Text className="text-xs text-gray-500">{desc}</Text>
+      <Image
+        source={{ uri: image }}
+        className="w-full h-32 rounded-lg"
+        resizeMode="cover"
+      />
+      <Text className="mt-2 font-semibold" numberOfLines={1}>
+        {name}
+      </Text>
+      <Text className="text-xs text-gray-500" numberOfLines={1}>
+        {desc}
+      </Text>
       <Text className="mt-1 font-medium">{price}</Text>
     </View>
   );
